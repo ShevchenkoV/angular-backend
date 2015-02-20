94f8ae1afc706b00443d32437f445927bf0448a3
Vitaliy <elanceShevchenko@gmail.com>
2015-02-21 01:54:07 +0200

    ticket:1 changed tests

--- django_angular_backend/app/tests.py
+++ django_angular_backend/app/tests.py
@@ -5,9 +5,7 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
 
 
 
@@ -18,10 +16,3 @@ class SimpleTest(TestCase):
         """
         self.assertEqual(1 + 1, 2)
 
-
-class HttpTest(TestCase):
-    def test_home(self):
-        c = Client()
-        response = c.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello!')
