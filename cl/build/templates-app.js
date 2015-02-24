c9d0983b45cab0daa285b93c707d8a4ae56b22be
Vitaliy <shevchenko-zp@yandex.ru>
2015-02-24 20:11:54 +0200

    ticket:1 remove files

--- cl/build/templates-app.js
+++
@@ -1,6 +0,0 @@
-angular.module('templates-app', ['home/home.tpl.html']);
-
-angular.module("home/home.tpl.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("home/home.tpl.html",
-    "<div><h1>home</h1></div>");
-}]);
